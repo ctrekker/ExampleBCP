2c5adbf4060f97618b26ee753202711392d01693
Connor Burns <ctrekker4@gmail.com>
2020-01-24 17:04:18 -0800

    Update to match BCP standards

--- program.js
+++ program.js
@@ -1,9 +1,10 @@
 let j = 0;
 let k = 1;
 for(let i=0; i<10; i++) {
-	console.log(j+k);
-	let t = k;
+	console.log(j);
+	let t = j;
 	j = k;
-	k = t;
+	k = t + k;
 }
 
+console.log('Current root:', process.cwd());
